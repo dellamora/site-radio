59aec2261573dbd6220ab263f783f5cdcfea501f
Dellamora <francielle@dellamora.dev>
2024-02-22 18:04:41 -0300

    feat: added PWA config

--- index.js
+++ index.js
@@ -90,3 +90,12 @@ function showDay(day) {
 }
 
 showCurrentDay();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
